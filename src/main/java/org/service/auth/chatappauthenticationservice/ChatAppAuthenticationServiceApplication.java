eb390bbe9ce8998af896ebe22004150f7495f85e
Tran Luu Dung <63690773+dungtl2003@users.noreply.github.com>
2024-03-01 23:28:13 +0700

    chore: init project

---
+++ src/main/java/org/service/auth/chatappauthenticationservice/ChatAppAuthenticationServiceApplication.java
@@ -0,0 +1,12 @@
+package org.service.auth.chatappauthenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppAuthenticationServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/org/service/auth/chatappauthenticationservice/ChatAppAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.service.auth.chatappauthenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppAuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
