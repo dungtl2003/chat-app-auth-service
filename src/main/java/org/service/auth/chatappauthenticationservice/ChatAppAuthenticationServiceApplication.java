f03ad615d8e720131c5f463a33e3bb034dbb0ae5
Tran Luu Dung <63690773+dungtl2003@users.noreply.github.com>
2024-03-03 01:01:16 +0700

    chore: update configs

--- src/main/java/org/service/auth/chatappauthenticationservice/ChatAppAuthenticationServiceApplication.java
+++ src/main/java/org/service/auth/chatappauthenticationservice/ChatAppAuthenticationServiceApplication.java
@@ -1,12 +1,15 @@
 package org.service.auth.chatappauthenticationservice;
 
+import org.service.auth.chatappauthenticationservice.configurations.PostgresqlConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(PostgresqlConfigProperties.class)
 public class ChatAppAuthenticationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChatAppAuthenticationServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChatAppAuthenticationServiceApplication.class, args);
+    }
 }
