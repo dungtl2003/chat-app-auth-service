c7005d4137fbfd98be792d7badc38af784256099
Tran Luu Dung <63690773+dungtl2003@users.noreply.github.com>
2024-03-04 21:45:07 +0700

    chore: add service anotation for UserService

--- src/main/java/org/service/auth/chatappauthservice/service/UserServiceImpl.java
+++ src/main/java/org/service/auth/chatappauthservice/service/UserServiceImpl.java
@@ -2,10 +2,12 @@ package org.service.auth.chatappauthservice.service;
 
 import lombok.AllArgsConstructor;
 import org.service.auth.chatappauthservice.repository.UserRepository;
+import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
+@Service
 public class UserServiceImpl implements UserService {
 
-	private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
 }
