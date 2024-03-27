9ab973288270579192c80b17898dadeb7d026940
Tran Luu Dung <63690773+dungtl2003@users.noreply.github.com>
2024-03-28 01:01:05 +0700

    fix: role converter try to convert null value to json

--- src/main/java/org/service/auth/chatappauthservice/utils/PrivacyConverter.java
+++ src/main/java/org/service/auth/chatappauthservice/utils/PrivacyConverter.java
@@ -6,23 +6,27 @@ import jakarta.persistence.AttributeConverter;
 
 public class PrivacyConverter implements AttributeConverter<String, String> {
 
-    private static final ObjectMapper mapper = new ObjectMapper();
+	private static final ObjectMapper mapper = new ObjectMapper();
 
-    @Override
-    public String convertToDatabaseColumn(String attribute) {
-        try {
-            return mapper.writeValueAsString(attribute);
-        } catch (JsonProcessingException e) {
-            return null;
-        }
-    }
+	@Override
+	public String convertToDatabaseColumn(String attribute) {
+		try {
+			return mapper.writeValueAsString(attribute);
+		}
+		catch (JsonProcessingException e) {
+			return null;
+		}
+	}
 
-    @Override
-    public String convertToEntityAttribute(String dbData) {
-        try {
-            return mapper.readValue(dbData, String.class);
-        } catch (JsonProcessingException e) {
-            return null;
-        }
-    }
+	@Override
+	public String convertToEntityAttribute(String dbData) {
+		try {
+			dbData = dbData != null ? dbData : "{}";
+			return mapper.readValue(dbData, String.class);
+		}
+		catch (JsonProcessingException e) {
+			return null;
+		}
+	}
+
 }
