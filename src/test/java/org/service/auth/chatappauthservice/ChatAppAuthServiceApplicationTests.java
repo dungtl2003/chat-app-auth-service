876d2adde37a516f9a73edcfe4263156990f4c91
Tran Luu Dung <63690773+dungtl2003@users.noreply.github.com>
2024-04-28 15:08:45 +0700

    feat: add more tests

--- src/test/java/org/service/auth/chatappauthservice/ChatAppAuthServiceApplicationTests.java
+++ src/test/java/org/service/auth/chatappauthservice/ChatAppAuthServiceApplicationTests.java
@@ -5,11 +5,15 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.service.auth.chatappauthservice.entity.User;
+import org.service.auth.chatappauthservice.entity.enums.TokenState;
+import org.service.auth.chatappauthservice.entity.enums.TokenType;
 import org.service.auth.chatappauthservice.service.AuthTokenService;
 import org.service.auth.chatappauthservice.service.UserService;
+import org.service.auth.chatappauthservice.utils.UserDTOMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +21,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -25,6 +30,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,6 +43,8 @@ class ChatAppAuthServiceApplicationTests {
 	private static String authenticateUrl;
 
 	private static String authorizationUrl;
+
+	private static String refreshUrl;
 
 	@LocalServerPort
 	private static int port;
@@ -54,6 +62,7 @@ class ChatAppAuthServiceApplicationTests {
     public static void setup() {
         authenticateUrl = STR."http://localhost:\{port}/api/v1/auth/login";
         authorizationUrl = STR."http://localhost:\{port}/api/v1/auth/authorize";
+        refreshUrl = STR."http://localhost:\{port}/api/v1/auth/refresh";
         tempUsers = getSampleUserFromJson();
     }
 
@@ -93,12 +102,30 @@ class ChatAppAuthServiceApplicationTests {
 		}
 	}
 
+	private String[] createUserRefreshTokensFamily(User user, long minExp, long maxExp, int numberOfTokens) {
+		if (minExp > maxExp || minExp < 0) {
+			throw new RuntimeException("Invalid min-max value");
+		}
+
+		if (numberOfTokens <= 0) {
+			throw new RuntimeException("Number of tokens should be greater than 0");
+		}
+
+		String[] refreshTokens = new String[numberOfTokens];
+		for (int i = 0; i < numberOfTokens; i++) {
+			refreshTokens[i] = authTokenService.createToken(new UserDTOMapper().apply(user),
+					minExp + Math.round(Math.random() * (maxExp - minExp)));
+		}
+
+		return refreshTokens;
+	}
+
 	private void addTempUsers() {
 		tempUsers.forEach(user -> userService.add(user.clone()));
 	}
 
 	private void removeTempUsers() {
-		tempUsers.forEach(user -> userService.delete(user.getUserId()));
+		tempUsers.forEach(user -> userService.deleteUserById(user.getUserId()));
 	}
 
 	@Test
@@ -249,26 +276,44 @@ class ChatAppAuthServiceApplicationTests {
 	}
 
 	@Test
-	public void testAuthorizeWithInvalidTokenShouldGet401UnauthorizedWithInvalidTokenMessage() {
-		try {
-			String body = mockMvc
-				.perform(MockMvcRequestBuilders.get(authorizationUrl).header("authorization", "Bearer faketokenhehehe"))
-				.andExpect(MockMvcResultMatchers.status().isUnauthorized())
-				.andReturn()
-				.getResponse()
-				.getContentAsString();
+    public void testAuthorizeWithInvalidTokenShouldGet401UnauthorizedWithInvalidTokenMessage() {
+        String fakeToken = "fakeheader.fakepayload.signature";
+        try {
+            String body = mockMvc
+                    .perform(MockMvcRequestBuilders.get(authorizationUrl).header("authorization", STR."Bearer \{fakeToken}"))
+                    .andExpect(MockMvcResultMatchers.status().isUnauthorized())
+                    .andReturn()
+                    .getResponse()
+                    .getContentAsString();
 
-			JsonNode bodyJson = convertStringToJson(body);
-			assertEquals("Invalid token", bodyJson.get("message").asText());
-		}
-		catch (Exception e) {
-			fail(e.getMessage());
-		}
-	}
+            JsonNode bodyJson = convertStringToJson(body);
+            assertEquals("Invalid token", bodyJson.get("message").asText());
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
 
 	@Test
-    public void testAuthorizeWithValidTokenShouldGet200WithAuthorizedMessage() {
-        String validToken = authTokenService.createAccessToken(tempUsers.getFirst());
+    public void testAuthorizeWithExpiredTokenShouldGet401UnauthorizedWithInvalidTokenMessage() {
+        String expiredToken = authTokenService.createToken(new UserDTOMapper().apply(tempUsers.getFirst()), 1);
+        try {
+            String body = mockMvc
+                    .perform(MockMvcRequestBuilders.get(authorizationUrl).header("authorization", STR."Bearer \{expiredToken}"))
+                    .andExpect(MockMvcResultMatchers.status().isUnauthorized())
+                    .andReturn()
+                    .getResponse()
+                    .getContentAsString();
+
+            JsonNode bodyJson = convertStringToJson(body);
+            assertEquals("Invalid token", bodyJson.get("message").asText());
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+	@Test
+    public void testAuthorizeWithValidTokenShouldGet200OkWithAuthorizedMessage() {
+        String validToken = authTokenService.createAccessToken(new UserDTOMapper().apply(tempUsers.getFirst()));
         try {
             String body = mockMvc
                     .perform(MockMvcRequestBuilders
@@ -283,5 +328,162 @@ class ChatAppAuthServiceApplicationTests {
             fail(e.getMessage());
         }
     }
+
+	@Test
+	public void testRefreshWithoutRefreshTokenShouldGet400BadRequestWithMissingRefreshTokenMessage() {
+		try {
+			String body = mockMvc.perform(MockMvcRequestBuilders.get(refreshUrl))
+				.andExpect(MockMvcResultMatchers.status().isBadRequest())
+				.andReturn()
+				.getResponse()
+				.getContentAsString();
+
+			JsonNode bodyJson = convertStringToJson(body);
+			assertEquals("Missing refresh token", bodyJson.get("message").asText());
+		}
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testRefreshWithInvalidRefreshTokenShouldGet404NotFoundWithInvalidRefreshTokenMessage() {
+		String fakeRefreshToken = "fakeheader.fakepayload.signature";
+		Cookie cookie = new Cookie("refresh_token", fakeRefreshToken);
+		cookie.setMaxAge(7 * 24 * 60 * 60);
+
+		try {
+			String body = mockMvc.perform(MockMvcRequestBuilders.get(refreshUrl).cookie(cookie))
+				.andExpect(MockMvcResultMatchers.status().isUnauthorized())
+				.andReturn()
+				.getResponse()
+				.getContentAsString();
+
+			JsonNode bodyJson = convertStringToJson(body);
+			assertEquals("Invalid refresh token", bodyJson.get("message").asText());
+		}
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testRefreshWithExpiredRefreshTokenShouldGet401UnauthorizedWithExpiredMessage() {
+		String[] refreshTokensFamily = createUserRefreshTokensFamily(tempUsers.getFirst(), 1, 1, 1);
+		tempUsers.getFirst().setRefreshTokens(refreshTokensFamily);
+
+		Cookie cookie = new Cookie("refresh_token", refreshTokensFamily[0]);
+		cookie.setMaxAge(7 * 24 * 60 * 60);
+
+		try {
+			addTempUsers();
+
+			String body = mockMvc.perform(MockMvcRequestBuilders.get(refreshUrl).cookie(cookie))
+				.andExpect(MockMvcResultMatchers.status().isUnauthorized())
+				.andReturn()
+				.getResponse()
+				.getContentAsString();
+
+			JsonNode bodyJson = convertStringToJson(body);
+			assertEquals("Token is expired", bodyJson.get("message").asText());
+		}
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+		finally {
+			removeTempUsers();
+		}
+	}
+
+	@Test
+	public void testRefreshWithValidRefreshTokenShouldProvideBothTokens() {
+		String refreshToken = authTokenService.createRefreshToken(new UserDTOMapper().apply(tempUsers.getFirst()));
+		String[] refreshTokens = new String[1];
+		refreshTokens[0] = refreshToken;
+		tempUsers.getFirst().setRefreshTokens(refreshTokens);
+
+		Cookie cookie = new Cookie("refresh_token", refreshToken);
+		cookie.setMaxAge(7 * 24 * 60 * 60);
+
+		try {
+			addTempUsers();
+
+			MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.get(refreshUrl).cookie(cookie))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.header().exists(HttpHeaders.SET_COOKIE))
+				.andReturn()
+				.getResponse();
+
+			String newRefreshToken = Objects.requireNonNull(response.getCookie("refresh_token")).getValue();
+			assertEquals(authTokenService.checkTokenState(newRefreshToken, TokenType.REFRESH_TOKEN), TokenState.VALID);
+			assertNotEquals(refreshToken, newRefreshToken);
+
+			String responseBody = response.getContentAsString();
+			JsonNode jsonBody = convertStringToJson(responseBody);
+			assertNotNull(jsonBody.get("access_token"));
+		}
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+		finally {
+			removeTempUsers();
+		}
+	}
+
+	@Test
+	public void testDetectReusedValidRefreshTokenShouldGet401UnauthorizedAndInvalidateAllDevices() {
+		String firstRefreshToken = authTokenService.createRefreshToken(new UserDTOMapper().apply(tempUsers.getFirst()));
+		String[] refreshTokens = new String[1];
+		refreshTokens[0] = firstRefreshToken;
+		tempUsers.getFirst().setRefreshTokens(refreshTokens);
+
+		try {
+			addTempUsers();
+
+			// normal user sends request with the first token (not used) should be fine
+			Cookie firstRTCookie = new Cookie("refresh_token", firstRefreshToken);
+			firstRTCookie.setMaxAge(7 * 24 * 60 * 60);
+			String secondRefreshToken = Objects
+				.requireNonNull(mockMvc.perform(MockMvcRequestBuilders.get(refreshUrl).cookie(firstRTCookie))
+					.andExpect(MockMvcResultMatchers.status().isOk())
+					.andReturn()
+					.getResponse()
+					.getCookie("refresh_token"))
+				.getValue();
+
+			// normal user sends next request with new refresh token should work normally
+			Cookie secondRTCookie = new Cookie("refresh_token", secondRefreshToken);
+			secondRTCookie.setMaxAge(7 * 24 * 60 * 60);
+			String thirdRefreshToken = Objects
+				.requireNonNull(mockMvc.perform(MockMvcRequestBuilders.get(refreshUrl).cookie(secondRTCookie))
+					.andExpect(MockMvcResultMatchers.status().isOk())
+					.andReturn()
+					.getResponse()
+					.getCookie("refresh_token"))
+				.getValue();
+
+			// hacker tries to reuse the second token
+			mockMvc.perform(MockMvcRequestBuilders.get(refreshUrl).cookie(secondRTCookie))
+				.andExpect(MockMvcResultMatchers.status().isUnauthorized())
+				.andReturn();
+
+			// now, no tokens should work
+			Cookie thirdRTCookie = new Cookie("refresh_token", thirdRefreshToken);
+			thirdRTCookie.setMaxAge(7 * 24 * 60 * 60);
+			mockMvc.perform(MockMvcRequestBuilders.get(refreshUrl).cookie(thirdRTCookie))
+				.andExpect(MockMvcResultMatchers.status().isUnauthorized())
+				.andReturn();
+
+			mockMvc.perform(MockMvcRequestBuilders.get(refreshUrl).cookie(firstRTCookie))
+				.andExpect(MockMvcResultMatchers.status().isUnauthorized())
+				.andReturn();
+		}
+		catch (Exception e) {
+			fail(e.getMessage());
+		}
+		finally {
+			removeTempUsers();
+		}
+	}
 
 }
