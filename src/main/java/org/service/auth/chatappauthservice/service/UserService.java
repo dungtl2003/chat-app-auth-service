721f7f9389ca901dc741dcc019bfa9775fa15889
Tran Luu Dung <63690773+dungtl2003@users.noreply.github.com>
2024-04-28 15:06:27 +0700

    feat: update user service
    add a refresh token to an user
    update user's refresh tokens
    get user's refresh tokens

--- src/main/java/org/service/auth/chatappauthservice/service/UserService.java
+++ src/main/java/org/service/auth/chatappauthservice/service/UserService.java
@@ -1,15 +1,22 @@
 package org.service.auth.chatappauthservice.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.service.auth.chatappauthservice.entity.User;
+import org.service.auth.chatappauthservice.exception.user.InvalidUserException;
+import org.service.auth.chatappauthservice.exception.user.UserNotFoundException;
 
 public interface UserService {
 
-	User getValidUser(String email, String password);
-
 	void add(User user);
 
-	void delete(Long userId);
+	void deleteUserById(long id);
 
-	void updateUserRefreshTokens(User user, String refreshToken);
+	User getValidUser(String email, String password) throws UserNotFoundException, InvalidUserException;
+
+	void addRefreshToken(long userId, String refreshToken);
+
+	void updateUserRefreshTokens(long userId, String[] refreshTokens);
+
+	String[] getUserRefreshTokens(long userId) throws EntityNotFoundException;
 
 }
