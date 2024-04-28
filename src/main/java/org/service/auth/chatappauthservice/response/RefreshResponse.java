0050adac90a236d2676bb7882c3eae1e5e458623
Tran Luu Dung <63690773+dungtl2003@users.noreply.github.com>
2024-04-28 14:59:21 +0700

    feat: add refresh response

--- src/main/java/org/service/auth/chatappauthservice/response/RefreshResponse.java
+++ src/main/java/org/service/auth/chatappauthservice/response/RefreshResponse.java
@@ -1,5 +1,6 @@
 package org.service.auth.chatappauthservice.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,6 +8,7 @@ import lombok.Data;
 @Builder
 public class RefreshResponse {
 
+	@JsonProperty("access_token")
 	private String accessToken;
 
 }
